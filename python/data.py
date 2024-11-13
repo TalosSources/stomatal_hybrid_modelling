progress in pipeline debugging, now correct order of magnitude but vastly incorrect values

python/data.py
@@ -27,7 +27,7 @@ def load_pipeline_data_dict(basepath, site_name): # NOTE: no real need to take 2
 
     # build a dictionary with the desired values and the correct names
     ctx = "sun_H" # NOTE: Change here for different contexts (sunny/shaded, low/high vegetation)
-    predictor_keys = ["Cc", "IPAR", "Csl", "ra", "rb", "Ts", "Pre", "Ds", "Psi_L", "Rn", "QG"]
+    predictor_keys = ["Cc", "IPAR", "Csl", "ra", "rb", "Ts", "Pre", "Ds", "Psi_L", "Rn", "QG", "rs"] # NOTE: added rs for debugging 
     constant_keys = ["Psi_sto_50", "Psi_sto_00", "CT", "Vmax", 
      "Ha", "FI", "Oa", "Do", "a1", "go", "gmes", "rjv"] # NOTE: missing DS. also why do we have integers for some constants?
     output_keys = ["LE"]
@@ -37,16 +37,28 @@ def load_pipeline_data_dict(basepath, site_name): # NOTE: no real need to take 2
     mapped_constant_keys = key_mapping(constant_keys, ctx)
     constants = {k : torch.tensor(predictor_data[k_m].astype(np.float32)).flatten() for k,k_m in zip(constant_keys, mapped_constant_keys)}
     output_arrays = {k : torch.tensor(observation_data[k].astype(np.float32)) for k in output_keys}
-    print(f"output_arrays={output_arrays}")
+    #print(f"output_arrays={output_arrays}")
+
+    print(f"n_points before filter: {len(predictor_arrays["Cc"])}")
+    
+    def valid(i):
+        return (
+            not torch.isnan(output_arrays[output_keys[0]][i])  # Filter out nan output values! 
+            and not predictor_arrays["Cc"][i] == 0
+            and not predictor_arrays["IPAR"][i] == 0
+        )
 
     # merge the dictionaries into an array of (input, output) tuples
     n = predictor_arrays[predictor_keys[0]].shape[0]
     data = [
         ({ k : predictor_arrays[k][i] for k in predictor_arrays } | constants | {"DS" : 0.5},
         output_arrays[output_keys[0]][i]) # For now, we assume a single output
-        for i in range(n)
-        if not torch.isnan(output_arrays[output_keys[0]][i]) # Filter out nan output values! 
+        for i in range(n) if valid(i) # Filter out nan output values! 
     ]
+
+    print(f"n_points after filter: {len(data)}")
+    k=10
+    print(f"first {k} data points:\n{data[:k]}")
 
     return data
 
@@ -87,7 +99,7 @@ def load_pipeline_data_tensor(basepath, site_name): # NOTE: no real need to take
     n = predictor_arrays[predictor_keys[0]].shape[0]
     batch_size = 16
     data = [
-        ({ k : predictor_arrays[k][i] for k in predictor_arrays } | constants | {"DS" : 0.5},
+        ({ k : predictor_arrays[k][i] for k in predictor_arrays } | constants | {"DS" : 0.5}, # NOTE: DS is arbitrary, because its use is cryptic in pb.m
         output_arrays[output_keys[0]][i]) # For now, we assume a single output
         for i in range(n)
     ]
@@ -127,6 +139,7 @@ sun_h_map = {
     "DS":"DSE_H", # NOTE: Hand-added
     "QG":"G", # NOTE: Hand-added
     "Rn":"Rn", # NOTE: Hand-added
+    "rs" : "rs_sunH", # NOTE: Hand-added
 }
 
 sun_l_map = {
@@ -154,4 +167,5 @@ sun_l_map = {
     "DS":"DSE_L", # NOTE: Hand-added
     "QG":"G", # NOTE: Hand-added
     "Rn":"Rn", # NOTE: Hand-added
+    "rs" : "rs_sunL", # NOTE: Hand-added
 }

python/differentiable_relations.py
@@ -14,6 +14,7 @@ def Vmax():
 cp = 1004.834
 specific_heat_air = 1.013e3  # J/kg/°C
 air_pressure_at_sea_level = 1.013e5  # Pa
+air_pressure_at_sea_level_kPa = air_pressure_at_sea_level / 1000 # kPa
 specific_gravity_water_vapor = 0.622  # unitless
 b0 = 1.91846e6
 b1 = -33.91
@@ -36,23 +37,43 @@ Parameters:
 
 Notes:
 * For now, we assume Tmean is 15 but it is to be replaced by a predictor
+* I think the given Ds is in Pa, while it should be in kPa here
+* this formula computes LE in W/m², which seems to be the ground truth values from T&C
+* ra and rs are probably of the right unit (except if stuff is passed by reference and modified)
+--> actually tensors seem to be partly passed by reference. check ra, rs and stuff keep their values
+* Rn and QG are probably okay
+* Ts is the important part:
+    * sc should be kPa/C (is C for celsius??)
+    * roa should be kg/m³
 """
 def Q_LE(rs, ra, Rn, QG, Ds, Ts):
-    lambda_ = b0 * np.power(Ts / (Ts + b1), 2)
-    gamma = specific_heat_air * air_pressure_at_sea_level / (specific_gravity_water_vapor * lambda_)
-    roa = air_pressure_at_sea_level / (287.05 * (Ts + zero_celsius_in_kelvin))
-    sc = compute_sc(Ts)
-    return (sc * (Rn - QG) + roa*cp*Ds / ra ) / (sc + gamma*(1 + rs/ra))
+
+    Ts_kelvin = Ts + zero_celsius_in_kelvin
+
+    # convert units
+    Ds_kPa = Ds / 1000
+
+    #print(f"computing Q_LE(rs={rs}, ra={ra}, Rn={Rn}, QG={QG}, Ds={Ds}, Ts={Ts})")
+    lambda_ = b0 * np.power(Ts_kelvin / (Ts_kelvin + b1), 2) # J/kg correct
+    gamma = specific_heat_air * air_pressure_at_sea_level_kPa / (specific_gravity_water_vapor * lambda_) # correct
+
+    # [Pa] / (constant [J / kg * K] * [K]) = [kg/m³]
+    roa = air_pressure_at_sea_level / (287.05 * Ts_kelvin) # kg / m³ probably correct
+
+
+    sc = compute_sc(Ts) # Pa / C
+    sc_kPa = sc / 1000 # kPa / C
+
+    q_le = (sc_kPa * (Rn - QG) + roa*cp*Ds_kPa / ra ) / (sc_kPa + gamma*(1 + rs/ra)) # flux, W/m²
+    #print(f"obtaining Q_LE={q_le}")
+    return q_le
 
 def compute_rs(Q_LE, ra, sc, Rn, QG, roa, cp, es, ea, gamma):
     return (ra*sc*(Rn - QG) + roa*cp*(es -ea) - ra*Q_LE*(sc + gamma) ) / ( gamma * Q_LE )
 
-def compute_rs_2(gsCO2, Tf, Ts, Pre, Pre0):
-    rsCO2=1/gsCO2
-    rsH20 = (rsCO2/1.64)*(1e6)
-    return rsH20*(Tf*Pre)/(0.0224*(Ts+273.15)*Pre0)
-
 # Taken from https://edis.ifas.ufl.edu/publication/AE459
+# Ts is in Celsius °C
+# Outputs sc in Pa/°C
 def compute_sc(Ts):
     b0, b1, b2 = 0.6108, 17.27, 237.3  # empirical coefficients
     saturated_vapour_pressure = 1e3 * b0 * np.exp(b1 * Ts / (b2 + Ts))  # Pa

python/train.py
@@ -8,41 +8,6 @@ import numpy as np
 import plot
 import inspect
 
-def train():
-
-    # training parameters
-    epochs = ...
-
-    # tensor containing the tunable parameters of the semi-empirical model
-    parameters = ...
-
-    # the loss used to compare ground-truth and predictions
-    criterion = ...
-
-    # gradient descent optimizer that adjusts the parameters
-    optimizer = ...
-
-    for i in range(epochs):
-
-        # load the arrays corresponding to function parameters and ground-truth output values
-        # (for example, some of the parameters of photosynthesis_biochemical, and gsCO2 values)
-        x = ...
-        y = ...
-
-
-        Cc,IPAR,Csl,ra,rb,Ts,Pre,Ds, Psi_L,Psi_sto_50,Psi_sto_00, CT,Vmax,DS,Ha,FI,Oa,Do,a1,go,gmes,rjv = x
-
-        result = photosynthesis_biochemical(Cc,IPAR,Csl,ra,rb,Ts,Pre,Ds, Psi_L,Psi_sto_50,Psi_sto_00, CT,Vmax,DS,Ha,FI,Oa,Do,a1,go,gmes,rjv)
-        CcF,An,rs,Rdark,F755nm,GAM,gsCO2 = result
-
-        loss = criterion(gsCO2, y)
-        loss.backward()
-        optimizer.step()
-        optimizer.zerograd()
-
-        ... # and so on, use some sensible gradient descent optimizer with some sensible loss
-
-
 def train_general(
     model,
     criterion,
@@ -74,13 +39,14 @@ def train_general(
         output = model(x)
         loss = criterion(output, y)
         print(f"Epoch {i}: x={x}, y={y}, output={output}, loss={loss}")
+        #print(f"Epoch {i}: y={y}, output={output}, loss={loss}")
         #print(f"Epoch {i}: loss={loss}")
         if torch.isnan(loss) or torch.isinf(loss) or (output==0.).any(): # TODO: very crude fix. Actually understand why having output=0 breaks the whole pipeline. Also filter out and sanitize data properly
             print(f"Found nan or inf loss: don't compute gradient")
             continue
             #return
-        loss.backward()
-        optimizer.step()
+        #loss.backward()
+        #optimizer.step()
 
         losses.append(float(loss))
 
@@ -95,7 +61,7 @@ def train_gsco2():
     loss = torch.nn.MSELoss()
     opt = torch.optim.Adam(model.parameters(), lr=3e-4)
     #opt = torch.optim.SGD(model.parameters(), lr=1e-2)
-    epochs = 2000
+    epochs = 10000
     data_iterator = gsco2_dummy_data_generator
     #data_iterator = simple_data_generator
 
@@ -138,7 +104,7 @@ def train_pipeline(train_data):
     
     gsCO2_model = models.gsCO2_model()
     #Vmax_model = models.vm_model()
-    Vmax_model = None # only train gsCO2 for now
+    Vmax_model = None # NOTE: only train gsCO2 for now
 
     """
     Need some function that computes pb outputs given chosen predictors. 
@@ -146,8 +112,7 @@ def train_pipeline(train_data):
     Else we should create another train_general function 
     """
     model_wrapper = make_pipeline(gsCO2_model, Vmax_model)
-    #data_iterator = iter(train_data)
-    data_iterator = batch_dict_iterator(train_data, batch_size=4)
+    data_iterator = batch_dict_iterator(train_data, batch_size=1)
 
     loss = torch.nn.MSELoss()
     #opt = torch.optim.Adam(gsCO2_model.parameters() + Vmax_model.parameters(), lr=3e-4)
@@ -155,7 +120,6 @@ def train_pipeline(train_data):
 
     epochs = 10000
 
-    # TODO: Use batches
     losses = train_general(model_wrapper, loss, opt, epochs, data_iterator)
 
     # show info about loss NOTE: Consider using wandb or similar foss
@@ -173,12 +137,15 @@ def make_pipeline(gsCO2_model, Vmax_model):
     # constants = ... # NOTE: Might not be necessary
 
     # the pipeline calls the pb module with the 2 models inserted, using the constants and predictors as inputs, and converts the outputs to Q_LE 
+    pb_params = set(inspect.signature(photosynthesis_biochemical).parameters)
+    qle_params = set(inspect.signature(differentiable_relations.Q_LE).parameters)
     def pipeline(predictors):
-        pb_params = set(inspect.signature(photosynthesis_biochemical).parameters)
         pb_predictors = {k: v for k, v in predictors.items() if k in pb_params}
         _,_,rs,_,_,_,_ = photosynthesis_biochemical(**pb_predictors, gsCO2_model=gsCO2_model, Vmax_model=Vmax_model)
-        qle_params = set(inspect.signature(differentiable_relations.Q_LE).parameters)
+        input_rs = predictors["rs"]
+        print(f"input_rs={input_rs} while output_rs = {rs}")
         qle_predictors = {k: v for k, v in predictors.items() if k in qle_params}
+        qle_predictors.__delitem__("rs") # TODO: temporary debug fix
         Q_LE = differentiable_relations.Q_LE(rs=rs, **qle_predictors)
         return Q_LE
 